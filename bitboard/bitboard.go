f0bc5385cd6ab8269b96b26a54d20e20dcb393f5
Abhishek Pratap Singh <abhi1kush@gmail.com>
2020-12-23 23:29:05 +0530

    Formated code.

--- bitboard/bitboard.go
+++ bitboard/bitboard.go
@@ -5,12 +5,22 @@ import (
 )
 
 const (
-	white = true
-	black = false
+	white              = true
+	black              = false
 	whitePieceIdxStart = 0
-	whitePieceIdxEnd = 15
+	whitePieceIdxEnd   = 15
 	blackPieceIdxStart = 16
-	blackPieceIdxEnd = 31
+	blackPieceIdxEnd   = 31
+)
+
+// Unique pieces.
+const (
+	king = iota
+	knight
+	queen
+	rook
+	bishop
+	pawn
 )
 
 //Pieces index
@@ -19,7 +29,7 @@ const (
 	knight1W
 	bBishopW
 	queenW
-	knighW 
+	kingW
 	wBishopW
 	knight2W
 	rook2W
@@ -43,7 +53,7 @@ const (
 	knight1B
 	bBishopB
 	queenB
-	knighB 
+	kingB
 	wBishopB
 	knight2B
 	rook2B
@@ -51,69 +61,68 @@ const (
 )
 
 type bitboard struct {
-	pieces [pieceCount]uint64 
+	pieces [pieceCount]uint64
 }
 
 type board struct {
-	sideToMove bool,
-	enPassantAvlbl bool,
-	wCastelLongAvlbl bool,
-	wCastelShortAvlbl bool,
-	bCastelLongAvlbl bool,
-	bCastelShortAvlbl bool, 
-	bitboard,
+	sideToMove        bool
+	enPassantAvlbl    bool
+	wCastelLongAvlbl  bool
+	wCastelShortAvlbl bool
+	bCastelLongAvlbl  bool
+	bCastelShortAvlbl bool
+	bitboard
 }
 
-
-func (b &bitboard) initBitBoard() {
+func (b bitboard) initBitBoard() {
 	var one uint64 = 1
 	b.pieces[rook1W] = one
-	b.pieces[knight1W] = one>>1
-	b.pieces[bBishopW] = one>>2
-	b.pieces[queenW] = one>>3
-	b.pieces[kingW] = one>>4
-	b.pieces[wBishopW] = one>>5
-	b.pieces[knight2W] = one>>6
-	b.pieces[rook2W] = one>>7
-	b.pieces[aPawnW] = one>>8
-	b.pieces[bPawnW] = one>>9
-	b.pieces[cPawnW] = one>>10
-	b.pieces[dPawnW] = one>>11
-	b.Pieces[ePawnW] = one>>12
-	b.pieces[fPawnW] = one>>13
-	b.Pieces[gPawnW] = one>>14
-	b.Pieces[hPawnW] = one>>15
-	var maxbit uint64 = uint64(math.Pow(2, 63)) 
+	b.pieces[knight1W] = one >> 1
+	b.pieces[bBishopW] = one >> 2
+	b.pieces[queenW] = one >> 3
+	b.pieces[kingW] = one >> 4
+	b.pieces[wBishopW] = one >> 5
+	b.pieces[knight2W] = one >> 6
+	b.pieces[rook2W] = one >> 7
+	b.pieces[aPawnW] = one >> 8
+	b.pieces[bPawnW] = one >> 9
+	b.pieces[cPawnW] = one >> 10
+	b.pieces[dPawnW] = one >> 11
+	b.pieces[ePawnW] = one >> 12
+	b.pieces[fPawnW] = one >> 13
+	b.pieces[gPawnW] = one >> 14
+	b.pieces[hPawnW] = one >> 15
+	var maxbit uint64 = uint64(math.Pow(2, 63))
 	b.pieces[rook2B] = maxbit
-	b.pieces[knight2B] = maxbit<<1
-	b.pieces[bBishopB] = maxbit<<2
-	b.Pieces[kingB] = maxbit<<3
-	b.Pieces[queenB] = maxbit<<4
-	b.Pieces[wBishopB] = maxbit<<5
-	b.Pieces[knight1B] = maxbit<<6
-	b.Pieces[rook1B] = maxbit<<7
-	b.Pieces[hPawnB] = maxbit<<8
-	b.Pieces[gPawnB] = maxbit<<9
-	b.Pieces[fPawnB] = maxbit<<10
-	b.Pieces[ePawnB] = maxbit<<11
-	b.Pieces[dPawnB] = maxbit<<12
-	b.Pieces[cPawnB] = maxbit<<13
-	b.Pieces[bPawnB] = maxbit<<14
-	b.Pieces[aPawnB] = maxbit<<15
+	b.pieces[knight2B] = maxbit << 1
+	b.pieces[bBishopB] = maxbit << 2
+	b.pieces[kingB] = maxbit << 3
+	b.pieces[queenB] = maxbit << 4
+	b.pieces[wBishopB] = maxbit << 5
+	b.pieces[knight1B] = maxbit << 6
+	b.pieces[rook1B] = maxbit << 7
+	b.pieces[hPawnB] = maxbit << 8
+	b.pieces[gPawnB] = maxbit << 9
+	b.pieces[fPawnB] = maxbit << 10
+	b.pieces[ePawnB] = maxbit << 11
+	b.pieces[dPawnB] = maxbit << 12
+	b.pieces[cPawnB] = maxbit << 13
+	b.pieces[bPawnB] = maxbit << 14
+	b.pieces[aPawnB] = maxbit << 15
 }
 
-func (b &board) init() {
+func (b board) init() {
 	b.sideToMove = white
 	b.enPassantAvlbl = false
-	wCastelLongAvlbl = true
-	wCastelShortAvlbl = true
-	bCastelLongAvlbl = true
-	bCastelShortAvlbl = true
+	b.wCastelLongAvlbl = true
+	b.wCastelShortAvlbl = true
+	b.bCastelLongAvlbl = true
+	b.bCastelShortAvlbl = true
 	b.bitboard.initBitBoard()
-} 
+}
 
 func bitToSquare(bitPos uint64) uint64 {
-	return math.Logb(bitPos)
+	return uint64(math.Logb(float64(bitPos)))
 }
 
 func squareToBit(squareNumber uint64) uint64 {
@@ -121,5 +130,5 @@ func squareToBit(squareNumber uint64) uint64 {
 	if squareNumber == 1 {
 		return one
 	}
-	return one>>(squareNumber - 1)
+	return one >> (squareNumber - 1)
 }
